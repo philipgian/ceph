419b7c516f0a4bc515c7f959ff0bbada80e32d69
Joao Eduardo Luis <joao.luis@inktank.com>
2014-06-11 16:22:39 +0100

    mon: MonitorDBStore: override generic bool leveldb options on init
    Although the default option for MonitorDBStore's compression was false,
    we were only overriding whatever was the default for the generic leveldb
    options IFF we happened to set said option to 'true'.  This would then
    degenerate in having this option being completely pointless if it was set
    to false (which was the default) and having leveldb compressing on the
    monitors.
    
    According to 'perf', there was a significant amount of time being spent
    compressing blocks during compaction.  After this patch, perf no longer
    showed any compression happening.
    
    Hopefully, this will reduce the cpu strain on big clusters.
    
    Signed-off-by: Joao Eduardo Luis <joao.luis@inktank.com>

--- src/mon/MonitorDBStore.h
+++ src/mon/MonitorDBStore.h
@@ -496,14 +496,16 @@ class MonitorDBStore
       db->options.block_size = g_conf->mon_leveldb_block_size;
     if (g_conf->mon_leveldb_bloom_size)
       db->options.bloom_size = g_conf->mon_leveldb_bloom_size;
-    if (g_conf->mon_leveldb_compression)
-      db->options.compression_enabled = g_conf->mon_leveldb_compression;
     if (g_conf->mon_leveldb_max_open_files)
       db->options.max_open_files = g_conf->mon_leveldb_max_open_files;
-    if (g_conf->mon_leveldb_paranoid)
-      db->options.paranoid_checks = g_conf->mon_leveldb_paranoid;
     if (g_conf->mon_leveldb_log.length())
       db->options.log_file = g_conf->mon_leveldb_log;
+
+    // These options are booleans and we must make sure they override
+    // whatever is the default for the common leveldb options; otherwise
+    // they will have no effect.
+    db->options.compression_enabled = g_conf->mon_leveldb_compression;
+    db->options.paranoid_checks = g_conf->mon_leveldb_paranoid;
   }
 
   int open(ostream &out) {
